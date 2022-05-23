8dfe7866c17bd04271d2c2a7546ee39392f38208
Nemo Thorx <nemo@thorx.net>
2022-05-23 21:16:34 +1000

    refactoring to my indent and loop style preference

--- ipconfig.sh
+++ ipconfig.sh
@@ -25,35 +25,33 @@ printf "\n"
 
 line_ct=$(ip --brief address show | wc -l)
 
-if [ $line_ct -gt 2 ]	#if more than one interface (&lo)
-then
-	for ((i=2;i<=$line_ct; i++))	#iterate over interfaces
-	do
-		let x=$i-1		#set if num as i-1
-		let color=($x-1)%7+1	#cycle color between 1 and 7
-		tput setaf $color	#change color for each if
-		echo -e "${uline}$x \n\tif:\t\tStatus:\t\tAddresses:${reset}"	#header
-		tput setaf 4		#ip=blue
-		printf "${fgblue}IP:\t"		#print if(i) ip
-		ip --brief address show | sed -n ${i}p
-		tput setaf 3		#mac=orange
-		printf "MAC:\t:"	#print if(i) MAC
-		ip --brief link | sed -n ${i}p
-		printf "\n"
-    	done
+#if more than one interface (&lo)
+if [ $line_ct -gt 2 ] ; then
+    #iterate over interfaces
+    for ((i=2;i<=$line_ct; i++)) ; do
+        let x=$i-1		#set if num as i-1
+        let color=($x-1)%7+1	#cycle color between 1 and 7
+        tput setaf $color	#change color for each if
+        echo -e "${uline}$x \n\tif:\t\tStatus:\t\tAddresses:${reset}"	#header
+        printf "${fgblue}IP:\t"		#print if(i) ip
+        ip --brief address show | sed -n ${i}p
+        printf "${fgyellow}MAC:\t:"	#print if(i) MAC
+        ip --brief link | sed -n ${i}p
+        printf "\n"
+    done
 else	#if only one if
-	printf "${uline}1:\tif:   \t\tStatus:\t\tAddresses:${reset}\n"	#header
-	printf "${fgblue}IP:\t"		#print IP
-	ip --brief address show | tail -1
-	printf "${fgyellow}MAC:\t"		#print MAC
-	ip --brief link | tail -1
+    printf "${uline}1:\tif:   \t\tStatus:\t\tAddresses:${reset}\n"	#header
+    printf "${fgblue}IP:\t"		#print IP
+    ip --brief address show | tail -1
+    printf "${fgyellow}MAC:\t"		#print MAC
+    ip --brief link | tail -1
 fi
 echo "${reset}"
 #default gateway info
 setterm -underline on
 echo -e "\n${fgteal}${uline}Default Gateway:${reset}"
 echo -n "${fgblue}$(ip -4 route show | head -1)" | sed 's/dev.*//' | sed 's/default via //'
-echo -e -n "\t\t" $(ip -6 route show | tail -1)"\n" | sed 's/dev.*//'
+echo -e -n "\t\t$(ip -6 route show | tail -1)\n" | sed 's/dev.*//'
 printf "\n"
 
 #dns info
